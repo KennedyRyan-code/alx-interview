00e0f35ae8ecc1fe425f57c1995a20e773d4a472
KennedyRyan-code <kenryan49@gmail.com>
2024-01-04 19:58:36 +0300

    Pascal_triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -11,11 +11,3 @@ def pascal_triangle(n):
         triangle.append(row)
 
     return triangle
-
-# Test the function
-def print_triangle(triangle):
-    for row in triangle:
-        print("[{}]".format(",".join([str(x) for x in row])))
-
-if __name__ == "__main__":
-    print_triangle(pascal_triangle(5))
