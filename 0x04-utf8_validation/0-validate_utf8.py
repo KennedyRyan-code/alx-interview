0777fa9349c76178797e3cccaaf224ae3b008f38
KennedyRyan-code <kenryan49@gmail.com>
2024-02-01 10:57:24 +0300

    UTF-8 Validation

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,28 @@
+def validUTF8(data):
+    # Determines if a given data set represents a valid utf-8 encoding
+    number_bytes = 0
+
+    for byte in data:
+        mask = 1 << 7  # Start with the leftmost bit
+
+        if number_bytes == 0:
+            while mask & byte:
+                number_bytes += 1
+                mask >>= 1  # Shift the mask to the right
+
+            if number_bytes == 0:
+                continue
+
+            if number_bytes == 1 or number_bytes > 4:
+                return False
+
+        else:
+            mask_1 = 1 << 7
+            mask_2 = 1 << 6
+
+            if not (byte & mask_1 and not (byte & mask_2)):
+                return False
+
+        number_bytes -= 1
+
+    return number_bytes == 0
